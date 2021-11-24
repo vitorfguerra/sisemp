7d9020df7d628b9bdda08ed937ad03234c3e7067
vitorfguerra <vitorfguerra@gmail.com>
2021-11-24 20:03:28 -0300

    1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import Flask, render_template, url_for
 
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
@@ -9,28 +10,34 @@ def get_db_connection():
 
 app = Flask('__name__')
 
+
 @app.route('/')
-def index():
+def base():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    return render_template('index.html', posts=posts)
-
-
+    return render_template('base.html', posts=posts)
 
 
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
 
+
 @app.route('/registrar')
 def registrar():
     return render_template('registrar.html')
+
 
 @app.route('/consultar')
 def consultar():
     return render_template('consultar.html')
 
+
 @app.route('/atualizar')
 def atualizar():
     return render_template('atualizar.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
